ac96d1b0fa2f7d050cfc76e306fc1798faef5ed3
lordprecious <lordprecious1@gmail.com>
2017-03-21 23:18:11 +0100

    get weather condition of your current location, and display result in sample front end

---
+++ js/weather.js
@@ -0,0 +1,88 @@
+var temp;
+var desc;
+var wSpeed;
+var loc;
+var icon;
+var hum;
+
+// my API key
+var myId = "82556f01ebbb43d4c57e8628105cc97e"
+
+
+// function to get weather info by city name
+function updateByName (name) {
+	var url = "http://api.openweathermap.org/data/2.5/weather?" + 
+				"q=" + name + "&appid=" + myId;
+	sendRequest(url);
+}
+
+
+// function to get weather info using current coordinates
+function updateByGeo (lat, lon) {
+	var url  = "http://api.openweathermap.org/data/2.5/weather?" +
+				"&lat=" + lat + "&lon=" + lon + "&appid=" + myId;
+	sendRequest(url);
+}
+
+function sendRequest (url) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var data = JSON.parse(xmlhttp.responseText);
+			var weather = {};
+			weather.loc = data.name;
+			weather.icon = data.weather[0].id;
+			weather.desc = data.weather[0].description;
+			weather.temp = tempToCelcius(data.main.temp);
+			weather.wSpeed = data.wind.speed;
+			weather.hum = data.main.humidity;
+			console.log(tempToCelcius(data.main.temp));
+			console.log(data.weather[0].description);
+			console.log(data.name);
+			console.log(data.wind.speed);
+			console.log(data.main.humidity);
+			updateWeather(weather);
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+
+// function to convert temperature from kelvin to celcius
+function tempToCelcius (kel) {
+	return Math.round(kel - 273.15)
+}
+
+function updateWeather (weather) {
+	//weda.innerHTML = weather;
+	loc.innerHTML = weather.loc;
+	desc.innerHTML = weather.desc;
+	temp.innerHTML = weather.temp;
+	wSpeed.innerHTML = weather.wSpeed;
+	hum.innerHTML = weather.hum;
+}
+
+// function to get the current cordinate
+function showPosition (position) {
+	updateByGeo(position.coords.latitude, position.coords.longitude);
+}
+
+window.onload = function () {
+	loc = document.getElementById("location");
+	// icon = document.getElementById("icon");
+	desc = document.getElementById("description");
+	temp = document.getElementById("temperature");
+	wSpeed = document.getElementById("windSpeed");
+	hum = document.getElementById("humidity");
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);	
+
+	} else {
+		var name = window.prompt("could not discover your location. What is your current city");
+		updateByName("jos");
+	}
+
+	
+}
